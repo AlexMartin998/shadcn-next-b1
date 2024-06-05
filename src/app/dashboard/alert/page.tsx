9c4836e957567bc99917236121a414edaaa8e7ee
Adrian <alextravc.342@gmail.com>
2024-06-04 19:30:04 -0500

    ADD: alert

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,5 +1,40 @@
+import { RocketIcon } from 'lucide-react';
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+
 const Page = () => {
-  return <>Alert page</>;
+  return (
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      {/* ================= */}
+      <Alert variant="destructive">
+        <RocketIcon className="h-4 w-4" />
+
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      {/* ================= */}
+      <Alert variant="success">
+        <RocketIcon className="h-4 w-4" />
+
+        <AlertTitle>Success!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 };
 
 export default Page;

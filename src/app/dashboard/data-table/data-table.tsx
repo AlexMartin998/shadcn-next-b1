0117052a9571759979ec3873f916c4f4bcb75dba
Adrian <alextravc.342@gmail.com>
2024-06-08 13:11:10 -0500

    ADD: Pagination - client side

--- src/app/dashboard/data-table/data-table.tsx
+++ src/app/dashboard/data-table/data-table.tsx
@@ -4,9 +4,12 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  // pagination -------
+  getPaginationRowModel,
   useReactTable,
 } from '@tanstack/react-table';
 
+import { Button } from '@/components/ui/button';
 import {
   Table,
   TableBody,
@@ -25,10 +28,14 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  // // Actual table Logic - Hook ===================================
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+
+    // pagination: Client Side -------
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
@@ -82,6 +89,39 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+
+      {/* =============================== PAGINATION =============================== */}
+      <div className="flex items-center justify-end space-x-2 py-4 mx-2">
+        {/* ---------------- ROWS COUNT ---------------- */}
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table?.getRowCount() > 0 && (
+            <span>
+              Showing {table?.getPaginationRowModel().rows.length} of{' '}
+              {table?.getRowCount()} rows
+            </span>
+          )}
+        </div>
+
+        {/* ---------------- PAGINATION BUTTONS ---------------- */}
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
